09858cf5b08c0e01fc79540180752df69704590f
Aditya Dhiman <209089842+tukkel23@users.noreply.github.com>
2025-10-17 17:48:08 +0530

    Add function for round

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+let computerChoice = 0;
+let humanChoice = 0;
+
 function getComputerChoice() {
     const compChoiceNumber = Math.floor(Math.random() * 3 + 1);
-    if (choiceNumber == 1) {
+    if (compChoiceNumber == 1) {
         return "rock"
-    } else if (choiceNumber == 2) {
+    } else if (compChoiceNumber == 2) {
         return "paper"
     } else {
         return "scissor"
@@ -10,13 +13,41 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoiceNumber = prompt("ENTER 1 FOR ROCK, 2 FOR PAPER OR 3 FOR SCISSORS").toLowerCase();
-    if (humanChoiceNumber == 1 || humanChoiceNumber = "rock") {
+    let humanChoiceNumber = prompt("ENTERROCK, PAPER OR SCISSORS").toLowerCase();
+    if (humanChoiceNumber = "rock") {
         return "rock"
-    } else if (humanChoiceNumber == 2 || humanChoiceNumber == "paper" || humanChoiceNumber == "papers") {
+    } else if (humanChoiceNumber == "paper" || humanChoiceNumber == "papers") {
         return "paper"
-    } else {
+    } else if (humanChoiceNumber == "scissor" || humanChoiceNumber == "scissors") {
         return "scissor"
     }
 
+}
+
+function playRound(humanChoice, computerChoice) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    if (humanChoice == "rock") {
+        if (computerChoice == "rock") {} else if (computerChoice == "scissor") {
+            humanChoice += 1;
+        } else {
+            computerChoice += 1;
+        }
+    }
+
+    if (humanChoice == "paper") {
+        if (computerChoice == "paper") {} else if (computerChoice == "rock") {
+            humanChoice += 1;
+        } else {
+            computerChoice += 1;
+        }
+    }
+
+    if (humanChoice == "scissor") {
+        if (computerChoice == "scissor") {} else if (computerChoice == "paper") {
+            humanChoice += 1;
+        } else {
+            computerChoice += 1;
+        }
+    }
 }
